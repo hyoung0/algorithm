202468baafcfc3d75b8ad87ae40758316f20c97d
hyoung0 <121420780+hyoung0@users.noreply.github.com>
2023-06-25 18:32:22 +0900

    [Bronze III] Title: 핸드폰 요금, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.py
+++ 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.py
@@ -1,16 +1,12 @@
 N = int(input())
-call = list(map(int, input().split()))
-
-y = 0 #영식 요금제
-m = 0 #민식 요금제
-for i in call:
-    y += (i//30+1)*10
-    m += (i//60+1)*15
-
-#출력
-if y > m:
-    print("M", m)
-elif y == m:
+li = list(map(int, input().split()))
+y = m = 0
+for n in li:
+    y += (n//30 + 1) * 10
+    m += (n//60 + 1) * 15
+if m == y:
     print("Y M", m)
+elif m < y:
+    print("M", m)
 else:
     print("Y", y)
