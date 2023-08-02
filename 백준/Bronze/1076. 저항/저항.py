44e6a723fd9baee16f16e53004c46d5909e95b49
hyoung0 <121420780+hyoung0@users.noreply.github.com>
2023-08-02 20:23:04 +0900

    [Bronze II] Title: 저항, Time: 48 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Bronze/1076. 저항/저항.py
@@ -0,0 +1,5 @@
+color = ["black", "brown", "red", "orange", "yellow", "green", "blue", "violet", "grey", "white"]
+input1 = input()
+input2 = input()
+input3 = input()
+print(int(str(color.index(input1))+str(color.index(input2)))*(10**color.index(input3)))
